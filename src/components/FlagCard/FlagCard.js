748681ee832178227ee6c18fa2a7da81b1e9b8f7
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-12-07 13:03:55 -0800

    added all continent options to be filtered

--- src/components/FlagCard/FlagCard.js
+++ src/components/FlagCard/FlagCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './FlagCard.css';
 
-export default function FlagCard({ name, iso2 }) {
+export default function FlagCard({ name, iso2, continent }) {
   return (
     <>
       <div className="flag">
         <img src={`https://flagcdn.com/80x60/${iso2.toLowerCase()}.png`} />
         <h2>{name}</h2>
+        <h3>{continent}</h3>
       </div>
     </>
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -43,13 +43,19 @@ export default function Main() {
           }}
         >
           <option value="all">All</option>
-          <option value="africa">Africa</option>
+          <option value="Asia">Asia</option>
+          <option value="Africa">Africa</option>
+          <option value="Antarctica">Antarctica</option>
+          <option value="Europe">Europe</option>
+          <option value="North America">North America</option>
+          <option value="South America">South America</option>
+          <option value="Oceania">Oceania</option>
         </select>
       </div>
 
       <div className="flag-display">
         {/* here is where you will replace flags.map with your filter function, in order to update page based on user interaction */}
-        {flags.map((flag) => {
+        {filterFlags().map((flag) => {
           return <FlagCard key={flag.name} {...flag} />;
         })}
       </div>
