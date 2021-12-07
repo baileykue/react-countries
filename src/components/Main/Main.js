0cd763cdef7e68a225db15dfdd68f66ea9ffa78b
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-12-07 12:42:51 -0800

    following along with julies demo, added some demo code to work from for filtering

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,6 +5,8 @@ import { getFlags } from '../../services/flags';
 
 export default function Main() {
   const [flags, setFlags] = useState([]);
+  const [query, setQuery] = useState('');
+  const [continent, setContinent] = useState('all');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,11 +16,43 @@ export default function Main() {
     fetchData();
   }, []);
 
+  const filterFlags = () => {
+    return flags.filter((flag) => {
+      return (
+        // here is where you will continue to list out different filtering and sorting methods
+        flag.name.includes(query) && (flag.continent === continent || continent === 'all')
+      );
+    });
+  };
+
   return (
-    <div className="flag-display">
-      {flags.map((flag) => {
-        return <FlagCard key={flag.name} {...flag} />;
-      })}
-    </div>
+    <>
+      <div>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+        />
+
+        <select
+          value={continent}
+          onChange={(e) => {
+            setContinent(e.target.value);
+          }}
+        >
+          <option value="all">All</option>
+          <option value="africa">Africa</option>
+        </select>
+      </div>
+
+      <div className="flag-display">
+        {/* here is where you will replace flags.map with your filter function, in order to update page based on user interaction */}
+        {flags.map((flag) => {
+          return <FlagCard key={flag.name} {...flag} />;
+        })}
+      </div>
+    </>
   );
 }
