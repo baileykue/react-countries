fdbe90390457f1d49b34130c373589b48fa5f9ce
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-12-07 17:35:37 -0800

    added final styling to page

--- src/components/FlagCard/FlagCard.js
+++ src/components/FlagCard/FlagCard.js
@@ -5,7 +5,7 @@ export default function FlagCard({ name, iso2, continent }) {
   return (
     <>
       <div className="flag">
-        <img src={`https://flagcdn.com/96x72/${iso2.toLowerCase()}.png`} />
+        <img src={`https://flagcdn.com/112x84/${iso2.toLowerCase()}.png`} />
         <h2>{name}</h2>
         <h4 className="italic">{continent}</h4>
       </div>
